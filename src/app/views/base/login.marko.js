e07ebe4bad166183e40b304abad38fa3a654d6bc
Evandro Ignacio <reignomo27@hotmail.com>
2019-05-20 17:54:53 -0300

    ..

--- src/app/views/base/login.marko.js
+++ src/app/views/base/login.marko.js
@@ -1,16 +1,31 @@
-// Compiled using marko@4.13.4-1 - DO NOT EDIT
+// Compiled using marko@4.16.15 - DO NOT EDIT
 "use strict";
 
 var marko_template = module.exports = require("marko/src/html").t(__filename),
-    marko_componentType = "/fc-fila$1.0.0/src/app/views/login.marko",
-    components_helpers = require("marko/src/components/helpers"),
+    marko_componentType = "/fc-fila$1.0.0/src/app/views/base/login.marko",
+    components_helpers = require("marko/src/runtime/components/helpers"),
     marko_renderer = components_helpers.r,
-    marko_defineComponent = components_helpers.c;
+    marko_defineComponent = components_helpers.c,
+    marko_helpers = require("marko/src/runtime/html/helpers"),
+    marko_loadTag = marko_helpers.t,
+    component_globals_tag = marko_loadTag(require("marko/src/core-tags/components/component-globals-tag")),
+    init_components_tag = marko_loadTag(require("marko/src/core-tags/components/init-components-tag")),
+    await_reorderer_tag = marko_loadTag(require("marko/src/core-tags/core/await/reorderer-renderer"));
 
 function render(input, out, __component, component, state) {
   var data = input;
 
-  out.w("<html><title>Login</title></html>");
+  out.w("<html><head><title>Login</title></head><body>");
+
+  component_globals_tag({}, out);
+
+  out.w("<a href=\"https://slack.com/oauth/authorize?scope=identity.basic&amp;client_id=78682276946.629095647621&amp;redirect_uri=https://ps4-fcamara.herokuapp.com/app/callback.php\"><img src=\"https://api.slack.com/img/sign_in_with_slack.png\"></a>");
+
+  init_components_tag({}, out);
+
+  await_reorderer_tag({}, out, __component, "6");
+
+  out.w("</body></html>");
 }
 
 marko_template._ = marko_renderer(render, {
@@ -21,5 +36,10 @@ marko_template._ = marko_renderer(render, {
 marko_template.Component = marko_defineComponent({}, marko_template._);
 
 marko_template.meta = {
-    id: "/fc-fila$1.0.0/src/app/views/login.marko"
+    id: "/fc-fila$1.0.0/src/app/views/base/login.marko",
+    tags: [
+      "marko/src/core-tags/components/component-globals-tag",
+      "marko/src/core-tags/components/init-components-tag",
+      "marko/src/core-tags/core/await/reorderer-renderer"
+    ]
   };
---
+++ src/config/callback.js
@@ -0,0 +1,6 @@
+const axios = require('axios');
+
+// This is the client ID and client secret that you obtained
+// while registering the application
+const clientID = '78682276946.629095647621';
+const clientSecret = '34216c65e65b23e6604f660d758938eb';
